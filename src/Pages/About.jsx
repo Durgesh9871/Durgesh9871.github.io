d8353b4a7e2628b5277cd83382f801c2f23adfc1
Durgesh9871 <devisingh9871686098@gmail.com>
2023-02-04 01:01:03 +0530

    file

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -3,6 +3,11 @@ import { EmailIcon } from '@chakra-ui/icons';
 import {Link,}
  from '@chakra-ui/react'
 import "../CssPages/About.css"
+import { PhoneIcon } from '@chakra-ui/icons';
+
+
+
+
 const About = () => {
   return (
     <div id='aboutScroll'>
@@ -18,8 +23,9 @@ const About = () => {
     <h1 >About</h1>
     <h2>I'm Durgesh Singh</h2>
     <p>Observant & detail-oriented aspiring full-stack web developer with a specialization in MERN stack. Judicious & creative when crafting web applications & platforms to propel competitive advantage & revenue growth. Looking forward to honing his skills in a challenging work environment.</p>
-<p>Drop a mail @ <Link href="mailto:aadarsh474747@gmail.com"><span style={{textDecoration:"underline",color:"salmon",cursor:"pointer"}}>
+<p>Drop a mail @ <Link href="mailto:aadarsh474747@gmail.com"><span style={{textDecoration:"underline",color:"salmon",cursor:"pointer" ,marginRight:"5px"}}>
     devisingh9871686098@gmail.com </span></Link> <EmailIcon/> </p>
+    <p style={{marginTop:"15px"}}>Contact me <span style={{textDecoration:"underline",color:"salmon",cursor:"pointer", marginLeft:"5px" ,marginRight:"5px" }}>  9871686098 </span> <PhoneIcon/> </p>
  </div>
     </div>
   )
