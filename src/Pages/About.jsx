617f8b6d3372217c2a7299dbf34c710e5fe1f082
Durgesh9871 <devisingh9871686098@gmail.com>
2023-02-28 09:50:57 +0530

    file

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -22,7 +22,7 @@ const About = () => {
     data-aos-anchor-placement="top-center">
     <h1 >About</h1>
     <h2>I'm Durgesh Singh</h2>
-    <p>& creative when crafting web applications & platforms to propel competitive advantage & revenue growth. Looking forward to honing his skills in a challenging work environment.</p>
+    <p>to honing his skills in a challenging work environment.</p>
 <p>Drop a mail @ <Link href="mailto:aadarsh474747@gmail.com"><span style={{textDecoration:"underline",color:"salmon",cursor:"pointer" ,marginRight:"5px"}}>
     devisingh9871686098@gmail.com </span></Link> <EmailIcon/> </p>
     <p style={{marginTop:"15px"}}>Contact me <span style={{textDecoration:"underline",color:"salmon",cursor:"pointer", marginLeft:"5px" ,marginRight:"5px" }}>  9871686098 </span> <PhoneIcon/> </p>
