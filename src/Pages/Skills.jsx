78970f9a9d191deb3c4e871426dfab79d7f04f9b
Durgesh9871 <devisingh9871686098@gmail.com>
2023-03-01 12:10:14 +0530

    file

--- src/Pages/Skills.jsx
+++ src/Pages/Skills.jsx
@@ -199,7 +199,17 @@ const Skills = () => {
                   <h1 style={{color:"#EBF8FF"}}>Github</h1>
                  </Box>
                  {/* Netlify */}
-                 
+                 <Box data-aos="fade-down"
+    data-aos-offset="-100"
+    data-aos-delay="50"
+    data-aos-duration="1000"
+    data-aos-easing="ease-in-out"
+    data-aos-mirror="true"
+    data-aos-once="false"
+    data-aos-anchor-placement="top-center" boxShadow= "4px 4px #EBF8FF, -0em 1px .4em #EBF8FF"  >
+                 <img style={{borderRadius:"50%"}} src="https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/512/external-netlify-a-cloud-computing-company-that-offers-hosting-and-serverless-backend-services-for-static-websites-logo-shadow-tal-revivo.png" />
+                  <h1 style={{color:"#EBF8FF"}}>Netlify</h1>
+                 </Box>
 
 
 
